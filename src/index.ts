60d349407c500b9b7f98eb3317c96f3a25746c89
leydijo <leydilenis85@gmail.com>
2023-11-11 22:46:17 -0500

    tercer commit

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import "dotenv/config";
 import app from "./app/app";
 import dbConnect from "./config/mysql";
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 // Conecta a la base de datos MySQL
 dbConnect()
